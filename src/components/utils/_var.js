96e6f47af22dc629bb5e6d231f1e7d409929413a
vaibhavr2107 <57088177+vaibhavr2107@users.noreply.github.com>
2021-08-11 13:42:41 +0530

    main

---
+++ src/components/utils/_var.js
@@ -0,0 +1,13 @@
+const fonts = {
+  $titleFont: 'Ubuntu, sans-serif',
+  $mainFont: 'Raleway, sans-serif',
+  $latoFont: 'Lato, sans-serif'
+}
+
+const colors = {
+  $colorBg: '#fff',
+  $colorCardBg: '#6448d4',
+  $colorGold: '#fff'
+}
+
+export { fonts, colors }
